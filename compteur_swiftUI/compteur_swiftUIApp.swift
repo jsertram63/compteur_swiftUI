3fab93cef013f8fa868d486d04494e94d16fc42c
Lunack <lunack@Mac-mini-de-Lunack.local>
2022-03-07 18:51:22 +0100

    first commit

---
+++ compteur_swiftUI/compteur_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  compteur_swiftUIApp.swift
+//  compteur_swiftUI
+//
+//  Created by Lunack on 07/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct compteur_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
