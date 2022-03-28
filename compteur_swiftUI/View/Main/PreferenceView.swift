3b99fc75304a7b4502a2c14142121b4d3f7c2e2c
jsertram63 <jser>
2022-03-28 19:38:57 +0200

    vue préférences

--- compteur_swiftUI/View/Main/PreferenceView.swift
+++ compteur_swiftUI/View/Main/PreferenceView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct PreferenceView: View {
+    private var libelles:[String] = ["Posts","Articles","Votes"]
+    @State var pas: Int = 1
+    
+    private var colors = ["Red", "Green", "Blue"] // <1>
+    @State private var selectedLibelleIndex = 0
     var body: some View {
+        
         
         ZStack {
             Color("Color1").ignoresSafeArea()
             
             VStack {
-                Text("Préférences")
+                HStack() {
+                    VStack(alignment: .leading) {
+                        Stepper("Pas : \(pas)",value: $pas,in: 1...100)
+                        Text("\(libelles[selectedLibelleIndex])")
+                        
+                        Picker("Sélectionez un libelle", selection: $selectedLibelleIndex, content: {
+                                    ForEach(0..<libelles.count, content: { index in // <2>
+                                        Text(libelles[index]) // <3>
+                                    })
+                                })
+                            }
+                    .padding()
+                    
+                }
+                .pickerStyle(WheelPickerStyle())
             }
             .foregroundColor(Color("Color5"))
         }
