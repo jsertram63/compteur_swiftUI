e673d139b4afba17d4ae76ce69d284cc1e621f04
PierricM380 <pierric.marye@wanadoo.fr>
2022-05-25 11:04:20 +0200

    Début d'implémentation d'un guide utilisateur

--- compteur_swiftUI/View/ContentView.swift
+++ compteur_swiftUI/View/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     @EnvironmentObject  private var compteurVM: CompteurViewModel
     @EnvironmentObject  private var PickerVM: PickerViewModel
     
+    @AppStorage("premiereUtilisation") var premiereUtilisation: Bool = false
+    
+    let screenSize = UIScreen.main.bounds
+    
     private var colorData = ColorData()
     
     // initialisation de la tabBar pour effectuer des modifications sur celle-ci
@@ -20,6 +24,36 @@ struct ContentView: View {
     }
     
     var body: some View {
+        // tabView
+        if premiereUtilisation {
+            tabView
+        } else {
+            accueilView
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(CompteurViewModel())
+            .environmentObject(TimerViewModel())
+            .environmentObject(PickerViewModel())
+    }
+}
+
+/* ******************************* Extension de View ************************************ */
+
+extension View {
+    func cacherClavier() {
+        let resign = #selector(UIResponder.resignFirstResponder)
+        UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
+    }
+}
+
+extension ContentView {
+    // TabView
+    private var tabView: some View {
         // Tabview : View container pour implémenter une tabbar (4 onglets)
         TabView {
             // onglet compteur
@@ -66,22 +100,67 @@ struct ContentView: View {
             compteurVM.arrierePlan = colorData.loadColor()
         }
     }
-}
+    
+    // Accueil
+    private var accueilView: some View {
+        TabView {
+            // Première page
+            VStack(spacing: 25.0) {
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .environmentObject(CompteurViewModel())
-            .environmentObject(TimerViewModel())
-            .environmentObject(PickerViewModel())
-    }
-}
+                Image("counter")
 
-/** Extension de View */
+                VStack(alignment: .center, spacing: 10.0) {
+                    Text("Bienvenue dans TouComptage.")
+                        .font(.title3)
+                        .fontWeight(.bold)
 
-extension View {
-    func cacherClavier() {
-        let resign = #selector(UIResponder.resignFirstResponder)
-        UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
+                    Text("Au travers ce petit didactiel, vous allez découvrir comment utiliser au mieux les différences fonctionnalités de notre application.")
+                        .fontWeight(.medium)
+
+                    Text("TouComptage va vous permettre de réaliser des comptages selon vos besoins, la possibilité d'avoir une minuterie et de paramétrer vos préférences selon vos goûts.")
+                        .fontWeight(.medium)
+                }
+                .multilineTextAlignment(.center)
+                .padding()
+            }
+            .frame(width: screenSize.width * 0.9, height: screenSize.height * 0.7)
+            .background(.regularMaterial)
+            .cornerRadius(15)
+            .shadow(radius: 5)
+            .padding()
+            
+            // Seconde page
+            VStack {
+                HStack {
+                    Text("Le compteur")
+                        .font(.title3)
+                    .fontWeight(.bold)
+                    
+                    Spacer()
+                }
+                
+                Divider().padding(.bottom)
+                
+                VStack(alignment: .center) {
+                    HStack(alignment: .center) {
+                        Image("Compteur")
+                            .resizable()
+                            .foregroundColor(.cyan)
+                            .frame(width: 150, height: 125)
+                        
+                        Image("Compteur2")
+                            .resizable()
+                            .foregroundColor(.cyan)
+                            .frame(width: 150, height: 125)
+                    }
+                }
+            }
+            .padding()
+            .frame(width: screenSize.width * 0.9, height: screenSize.height * 0.7)
+            .background(.regularMaterial)
+            .cornerRadius(25)
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .background(compteurVM.arrierePlan.ignoresSafeArea())
     }
 }
